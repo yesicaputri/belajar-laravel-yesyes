cfe348161935d8091a12929b4b72829b957c748e
yesicaputri <yesicaputri16@gmail.com>
2022-11-16 16:41:21 +0700

    Tugas Yesica

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AkunController;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\ToDoController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\KelasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -124,10 +126,24 @@ use App\Http\Controllers\ToDoController;
     Route::get('/account/show', [AccountController::class, 'show']
     )->name('showAccount');
 
+// Route::get('/student', [SiswaController::class, 'index'])->name('studentIndex');
+// Route::get('/student/create', [SiswaController::class, 'create'])->name('studentCreate');
+// Route::post('/student', [SiswaController::class, 'store'])->name('studentStore');
+// Route::get('/student/(id)', [SiswaController::class, 'show'])->name('studentShow');
+// Route::get('/student/(id)/edit', [SiswaController::class, 'edit'])->name('studentEdit');
+// Route::put('/student/(id)', [SiswaController::class, 'update'])->name('studentUpdate');
+// Route::delete('/student/(id)', [SiswaController::class, 'destory'])->name('studentDestory');
 
+// Route::controller(SiswaController::class)->group(function () {
+//   Route::get('/student', 'index')->name('studentIndex');
+//   Route::get('/student/create', 'create')->name('studentCreate');
+//   Route::post('/student', 'store')->name('studentStore');
+//   Route::get('/student/(id)', 'show')->name('studentShow');
+//   Route::get('/student/(id)/edit', 'edit')->name('studentEdit');
+//   Route::put('/student/(id)', 'update')->name('studentUpdate');
+//   Route::delete('/student/(id)', 'destory')->name('studentDestory');
+//});
 
+Route::resource('student', SiswaController::class);
 
-
-
-    Route::get('/data-table', [AkunController::class, 'index'])->name('indexAkun');
-    Route::get('/table', [AkunController::class, 'create'])->name('createAkun');
+Route::resource('kelas', KelasController::class);
